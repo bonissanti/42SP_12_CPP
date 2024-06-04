29b4c5310a556ea63cb4b0dc2ce375dc145d2826
bonissanti <brunoonissanti@gmail.com>
2024-06-04 19:34:50 -0300

    CPP03: ex00, ex01, ex02 ok

--- CPP03/ex03/src/DiamondTrap.cpp
+++ CPP03/ex03/src/DiamondTrap.cpp
@@ -19,26 +19,21 @@
 
 #include "../include/DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(){
-	std::cout << "Default constructor called" << std::endl;
-}
-
-DiamondTrap::DiamondTrap(const std::string& str) : str(attribute),
-	hitPoints(10), energyPoints(10), attackDamage(0){
-		std::cout << "Parametrized constructor called" << std::endl;
+DiamondTrap::DiamondTrap(const std::string& name) : ClapTrap(name){
+		std::cout << "<DiamondTrap> default constructor called" << std::endl;
 }
 
 DiamondTrap::~DiamondTrap(){
-	std::cout << "ClapTrap destructor called" << std::endl;
+	std::cout << "<DiamondTrap> destructor called" << std::endl;
 }
 
 DiamondTrap::DiamondTrap(const DiamondTrap& toCopy){
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << "<DiamondTrap> copy constructor called" << std::endl;
 		*this = toCopy;
 }
 
 DiamondTrap& DiamondTrap::operator=(const DiamondTrap& toCopy){
-	std::cout << "Copy assignment operator called" << std::endl;
+	std::cout << "<DiamondTrap> copy assignment operator called" << std::endl;
 	if (this != &toCopy)
 		this->name = toCopy.name;
 	return (*this);
