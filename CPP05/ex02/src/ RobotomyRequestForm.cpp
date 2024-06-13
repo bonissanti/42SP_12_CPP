6486b22f543aba016fde9dd9b046fd40a43885e9
Bruno Onissanti <brunoonissanti@gmail.com>
2024-06-12 22:31:35 -0300

    CPP05 - ex00 / ex01

---
+++ CPP05/ex02/src/ RobotomyRequestForm.cpp
@@ -0,0 +1,73 @@
+/*************************************************************************************/
+/*   ⠀⠀  ⠀⠀⠀⠀ ⣴⣿⣦ ⠀⠀⠀⠀⠀⠀⠀⠀                                                       */
+/* ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀                                                       */
+/* ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣀⠀⠀⠀⠀⠀⠀⠀⠀                                                       */
+/* ⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣦          ⠀                                                   */
+/* ⠀⠀⠀⠀⠀⠀⣴⣿⢿⣷⠒⠲⣾⣾⣿⣿⠂         Created by: brunrodr - 06/12/2024                   */
+/* ⠀⠀⠀⠀⣴⣿⠟⠁⠀⢿⣿⠁⣿⣿⣿⠻⣿⣄⠀⠀⠀⠀   Updated by: brunrodr - 06/12/2024                   */
+/* ⠀⠀⣠⡾⠟⠁⠀⠀⠀⢸⣿⣸⣿⣿⣿⣆⠙⢿⣷⡀⠀⠀                                                       */
+/* ⣰⡿⠋⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⠀⠀⠉⠻⣿⡀                                                       */
+/* ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣆ ⠀       Email: brunrodr@student.42sp.org.br                 */
+/* ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⡿⣿⣿⣿⣿⡄⠀⠀⠀⠀                                                       */
+/* ⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠿⠟⠀⠀⠻⣿⣿⡇⠀⠀⠀⠀   ███████╗██╗██╗██╗██╗██╗██╗██╗██╗██╗   ██╗██╗   ██╗  */
+/* ⠀⠀⠀⠀⠀⠀⢀⣾⡿⠃⠀⠀⠀⠀⠀⠘⢿⣿⡀⠀⠀⠀   ██╔════╝██║██║██║██║██║██║██║██║██║   ██║██║   ██║  */
+/* ⠀⠀⠀⠀⠀⣰⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⡀⠀⠀   ███████╗██║██║██║██║██║██║██║██║██║   ██║██║   ██║  */
+/* ⠀⠀⠀⠀⢠⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣧⠀⠀   ╚════██║██║██║██║██║██║██║██║██║██║   ██║██║   ██║  */
+/* ⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣆⠀   ███████║██║██║██║██║██║██║██║██║╚██████╔╝╚██████╔╝  */
+/*  ⠀⠠⢾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⡤  ╚══════╝╚═╝╚═╝╚═╝╚═╝╚═╝╚═╝╚═╝╚═╝ ╚═════╝  ╚═════╝   */
+/*************************************************************************************/
+
+#include "../include/RobotomyRequestForm.hpp"
+
+Robotomy::Robotomy() : AForm("Robotomy form", 72, 45), target("House") {
+	debugMode(3, "<ROBOTOMY> Default Constructor called"); 
+}
+
+Robotomy::Robotomy(const std::string& target) : AForm("Robotomy form", 72, 45), target(target)
+{
+	debugMode(3, "<ROBOTOMY> Parametrized constructor called");
+	if (this->getToSign() < 1 || this->getExecute() < 1)
+		throw Robotomy::GradeTooHighException();
+	
+	else if (this->getToSign() > 72 || this->getExecute() > 45)
+		throw Robotomy::GradeTooLowException();
+}
+
+Robotomy::~Robotomy(){
+	debugMode(3, "<ROBOTOMY> Destructor called");
+}
+
+Robotomy::Robotomy(const Robotomy& toCopy) : AForm("Robotomy form", 72, 45), target(toCopy.target){
+	debugMode(3, "<ROBOTOMY> Copy constructor called");
+	if (this != &toCopy)
+		*this = toCopy;
+}
+
+Robotomy& Robotomy::operator=(const Robotomy& toCopy){
+	debugMode(3, "<ROBOTOMY> Copy assignment operator called");
+	if (this != &toCopy)
+		this->target = toCopy.target;
+	return (*this);
+}
+
+void	Robotomy::execute(Bureaucrat const& executor) const
+{
+	try
+	{
+		if (executor.getGrade() > this->getExecute())
+			throw Robotomy::GradeTooLowException();
+
+		else if (!this->getBoolSign())
+			throw Robotomy::NotSigned();
+	}
+	catch(const std::exception& e)
+	{
+		//TODO: bad message here
+		std::cout << BRED << this->target << " couldn't execute '" << this->getName() << "' because " 
+			<< e.what() << RESET << std::endl;
+		return ;
+	}
+	executor.executeForm(*this);
+	//TODO: good message here
+}
+
